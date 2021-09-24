d5f7024ac108e6cec7950a670228b8a27a14e2a6
KirillSmirnovff <smirnovk.1990@gmail.com>
2021-09-24 13:17:01 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,9 +4,16 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         System.out.println("Размер массива равен: " + ages.length);
-        String[] names = new String[100500];
-        System.out.println("Размер массива равен: " + names.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Kirill Smirnov";
+        names[1] = "Mikhail Kulikov";
+        names[2] = "Pavel Bashnyak";
+        names[3] = "Elena Rogozova";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
